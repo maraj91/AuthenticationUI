6019588be1bb1db26093b13ef6390a0b0c3dfec2
maraj hussain <marajh@leewayhertz.com>
2025-02-28 17:54:11 +0530

    Initial Commit

---
+++ AuthenticationUI/AuthenticationUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationUIApp.swift
+//  AuthenticationUI
+//
+//  Created by Maraj Hussain on 28/02/25.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticationUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
